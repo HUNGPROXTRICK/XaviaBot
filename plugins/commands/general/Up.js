a5e559214ef2f0edf92abc09b6023b58f9a1b7ba
HUNGXPRO <108609419+HUNGPROXTRICK@users.noreply.github.com>
2024-03-23 15:07:17 +0800

    Create Up.js

---
+++ plugins/commands/general/Up.js
@@ -0,0 +1,48 @@
+const config = {
+  name: "up",
+  aliases: ["upt"],
+  credits: "MiSao",
+};
+
+async function onCall({ message }) {
+  const uptimeInSeconds = process.uptime();
+  const hours = Math.floor(uptimeInSeconds / 3600);
+  const minutes = Math.floor((uptimeInSeconds % 3600) / 60);
+  const seconds = Math.floor(uptimeInSeconds % 60);
+  const Cpu =
+    Math.floor(uptimeInSeconds % 9);
+  const Ram =
+    Math.floor(uptimeInSeconds % 30);
+  const Ping =
+    Math.floor(uptimeInSeconds % 76);
+  
+
+  try {
+    const replyMessage = await message.reply(`
+【ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ʙᴇ́ sᴀɴɢ ᴄᴜᴛᴇᴇ_🎶】
+🐉『BOT NHU CAC』🐉
+═════════════════
+⏰【ʙᴏᴛ ᴢᴇɴɪᴛsᴜ ᴆᴀ̃ ᴏɴʟɪɴᴇ ᴆᴜ̛ᴏ̛̣ᴄ】⏰『${hours} 𝑮𝒊𝒐̛̀ ┇ ${minutes} 𝑷𝒉𝒖́𝒕 ┇ ${seconds} 𝑮𝑰𝑨̂𝒀』
+━━━━━━━━━━━━━━━━━
+ ⚙️ᴛʜᴏ̂ɴɢ ᴛɪɴ ʜᴇ̣̂ ᴛʜᴏ̂́ɴɢ⚙️
+『\❯𝐶𝑝𝑢 𝑢𝑠𝑎𝑔𝑒: ${Cpu}%』☄️
+『\❯𝑅𝑎𝑚 𝑢𝑠𝑎𝑔𝑒: ${Ram}%』🌌
+『\❯𝑃𝑖𝑛𝑔: ${Ping}𝑚𝑠』🌠
+【🌟ᴡɪsʜ ʏᴏᴜ ᴀ ʜᴀᴘᴘʏ ᴅᴀʏ ᴡɪᴛʜ ʙᴏᴛ ᴡᴀʀ🌟】
+═════════════════
+【ᴛʜᴏ̂ɴɢ ᴛɪɴ ʟɪᴇ̂ɴ ʜᴇ̣̂ ᴀᴅᴍɪɴ】👑
+━━━━━━━━━━━━━━━━━
+『⚜️【ғᴀᴄᴇʙᴏᴏᴋ】:https://www.facebook.com/mhungdev⚜️』
+═════════════════
+【ɢᴍᴀɪʟ ᴀᴅ】:【ccc👑】`);
+    
+    console.log(replyMessage);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default {
+  config,
+  onCall,
+};
